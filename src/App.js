98d9b7c1a485033e4df89db59755fd132044e09b
Ivan Kovba <kovbaivan11112@gmail.com>
2023-03-26 14:12:26 +0300

    add HeroBanner component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import './App.css';
+import { Footer, Navbar } from './components';
+import { ExerciseDetail, Home } from './pages';
+
 const App = () => {
-	return <div>App </div>;
+	return (
+		<Box width='400px' sx={{ width: { xl: '1448px' } }} m='auto'>
+			<Navbar />
+			<Routes>
+				<Route path='/' element={<Home />} />
+			</Routes>
+			<Routes>
+				<Route path='/exercise/:id' element={<ExerciseDetail />} />
+			</Routes>
+			<Footer />
+		</Box>
+	);
 };
 
 export default App;
